7f3e1e324dd450c7b907a122f95029bb38c7325a
Chirag Dara <chiragdara23@gmail.com>
2022-04-02 00:21:09 -0500

    initial commit - Login and Signup functionality

---
+++ atlanta-mission/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBU4znGWcO90wq-18gxbOhuQyvIt5SpG_Q",
+  authDomain: "atlantamission-931c6.firebaseapp.com",
+  projectId: "atlantamission-931c6",
+  storageBucket: "atlantamission-931c6.appspot.com",
+  messagingSenderId: "608253806067",
+  appId: "1:608253806067:web:3e2ae7f2618a2fdc26fe86",
+  measurementId: "G-M6WQPGJ4DT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
